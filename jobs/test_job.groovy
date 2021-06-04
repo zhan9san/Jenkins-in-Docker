3f582622c61ada06f3e9b6ed6c7d3aea69c5f253
Jack Zhang <jack4zhang@gmail.com>
2021-06-04 18:48:04 +0800

    Fix JENKINS-48571 Could not determine exact tip revision of <branch>

--- jobs/test_job.groovy
+++ jobs/test_job.groovy
@@ -3,6 +3,8 @@ multibranchPipelineJob('test-pipeline') {
     branchSource{
       source {
         git {
+          // https://issues.jenkins.io/browse/JENKINS-48571
+          id("owner-${project_folder}:repo-${project_name}") // IMPORTANT: use a constant and unique identifier
           remote('https://github.com/zhan9san/test-pipeline')
           traits {
             gitBranchDiscovery()
