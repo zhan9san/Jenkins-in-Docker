c6c5de3b19a535b1ff1eab1b4f606327b9ac3597
Jack Zhang <jack4zhang@gmail.com>
2021-07-01 18:11:06 +0800

    Update Seed job repo

--- seedjob.groovy
+++ seedjob.groovy
@@ -2,12 +2,15 @@ job('Job_DSL_Seed') {
   scm {
     git {
       remote {
-        url 'https://github.com/zhan9san/Jenkins-in-Docker.git'
+        url 'https://github.com/zhan9san/Jenkins-Seed-Job.git'
       }
     }
   }
   steps {
     jobDsl {
+      removedConfigFilesAction('DELETE')
+      removedJobAction('DELETE')
+      removedViewAction('DELETE')
       targets 'jobs/**/*.groovy'
     }
   }
